b69601a40ffe434a1d8457253d67dfa902b862fd
hvck <i@hvck.dev>
2026-02-28 19:31:07 +0300

    fix

--- server/src/db/client.ts
+++ server/src/db/client.ts
@@ -1,9 +1,3 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient({
-   datasources: {
-      db: {
-         url: process.env.DATABASE_URL,
-      },
-   },
-} as any);
+export const prisma = new PrismaClient();
