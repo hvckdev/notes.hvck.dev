4edf9fdac7f1b0ae54b7299c9a417e290d793a52
hvck <i@hvck.dev>
2026-02-28 19:13:30 +0300

    update prisma

--- server/src/db/client.ts
+++ server/src/db/client.ts
@@ -1,9 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
-// Prisma 7 сама подхватит DATABASE_URL из окружения, 
-// если мы передадим его в конструктор вот так:
 export const prisma = new PrismaClient({
-  datasource: {
+  datasources: {
     db: {
       url: process.env.DATABASE_URL,
     },
